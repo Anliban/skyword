0e7e224064e725d67806af0ebbafa198e2c6a8d9
BaeJang <qowod77@naver.com>
2019-12-26 03:14:23 +0900

    send message activity 추가

--- app/src/main/java/anliban/skyword/presenter/view/SendMessageActivity.kt
+++ app/src/main/java/anliban/skyword/presenter/view/SendMessageActivity.kt
@@ -1,2 +1,17 @@
 package anliban.skyword.presenter.view
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import anliban.skyword.R
+import anliban.skyword.databinding.ActivitySendMessageBinding
+
+class SendMessageActivity : AppCompatActivity(), IDataBinding {
+
+    override val resourceId = R.layout.activity_send_message
+    private val binding by lazy { bind<ActivitySendMessageBinding>(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.text.text = "send message"
+    }
+}
