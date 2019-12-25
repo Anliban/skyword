f0fa81ef6c016258f632932c96f7db45a12249fa
BaeJang <qowod77@naver.com>
2019-12-25 23:49:36 +0900

    local 데이터소스 작성

---
+++ app/src/main/java/anliban/skyword/data/source/local/LocalDataSource.kt
@@ -0,0 +1,42 @@
+package anliban.skyword.data.source.local
+
+import android.content.Context
+import anliban.skyword.data.User
+
+interface ILocalDataSource {
+
+    var fcmToken: String?
+    var email: String?
+    var password: String?
+    val user: User
+}
+
+class LocalDataSource(private val pref: PrefModel) : ILocalDataSource {
+
+    override var fcmToken: String?
+        get() = pref.fcmToken
+        set(value) {
+            pref.fcmToken = value
+        }
+
+    override var email: String?
+        get() = pref.email
+        set(value) {
+            pref.email = value
+        }
+
+    override var password: String?
+        get() = pref.password
+        set(value) {
+            pref.password = value
+        }
+
+    override val user: User
+        get() = User(pref.email!!, pref.password!!, pref.fcmToken)
+}
+
+class PrefModel(context: Context) : PreferenceModel(context) {
+    var fcmToken by stringPreference("fcm_token")
+    var email by stringPreference("email", User.INVALID_EMAIL)
+    var password by stringPreference("password", User.INVALID_PASSWORD)
+}
