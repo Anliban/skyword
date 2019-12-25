8d1065361ab201b848ed617fd24a33800b98e3ed
BaeJang <qowod77@naver.com>
2019-12-25 15:06:14 +0900

    first commit

---
+++ app/src/main/java/anliban/skyword/MainActivity.kt
@@ -0,0 +1,12 @@
+package anliban.skyword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
