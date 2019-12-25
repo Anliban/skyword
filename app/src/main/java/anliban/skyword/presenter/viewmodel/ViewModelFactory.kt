e75025834977309751c986f03d49a3a0154b66b5
BaeJang <qowod77@naver.com>
2019-12-26 03:11:25 +0900

    login, join viewmodel factory 추가

--- app/src/main/java/anliban/skyword/presenter/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/anliban/skyword/presenter/viewmodel/ViewModelFactory.kt
@@ -2,6 +2,8 @@ package anliban.skyword.presenter.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import anliban.skyword.repository.IJoinRepository
+import anliban.skyword.repository.ILoginRepository
 import anliban.skyword.repository.ISplashRepository
 
 class SplashViewModelFactory(
@@ -9,5 +11,21 @@ class SplashViewModelFactory(
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST") return SplashViewModel(repository) as T
+    }
+}
+
+class LoginViewModelFactory(
+    private val repository: ILoginRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST") return LoginViewModel(repository) as T
+    }
+}
+
+class JoinViewModelFactory(
+    private val repository: IJoinRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST") return JoinViewModel(repository) as T
     }
 }
