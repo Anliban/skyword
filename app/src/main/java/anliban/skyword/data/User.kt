c36dc30af84402e5af0658142a84d7deff946b51
BaeJang <qowod77@naver.com>
2019-12-26 03:08:44 +0900

    User 데이터 변경

--- app/src/main/java/anliban/skyword/data/User.kt
+++ app/src/main/java/anliban/skyword/data/User.kt
@@ -1,12 +1,19 @@
 package anliban.skyword.data
 
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class User(
     val email: String = INVALID_EMAIL,
-    val password: String = INVALID_PASSWORD,
-    val token: String?
+    @Exclude val password: String = INVALID_PASSWORD,
+    val reward: Int = 0
 ) {
+    @Exclude
     fun isLogin() =
-        email != INVALID_EMAIL && password != INVALID_PASSWORD && token != null
+        email != INVALID_EMAIL && password != INVALID_PASSWORD
+
+    fun convertedEmail() = email.replace(".", "*")
 
     companion object {
         const val INVALID_EMAIL = "email null!!!!!!!!!!"
