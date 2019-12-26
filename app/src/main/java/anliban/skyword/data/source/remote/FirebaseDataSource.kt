0701f148c4a2dc6819a697bd2a14e178d59f2f1f
BaeJang <qowod77@naver.com>
2019-12-27 02:59:20 +0900

    firebase database getUser, updateUser 추가

--- app/src/main/java/anliban/skyword/data/source/remote/FirebaseDataSource.kt
+++ app/src/main/java/anliban/skyword/data/source/remote/FirebaseDataSource.kt
@@ -4,8 +4,13 @@ import anliban.skyword.data.Result
 import anliban.skyword.data.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -89,6 +94,10 @@ class FirebaseAuthDataSource(
 interface IFirebaseDbDataSource {
 
     suspend fun saveUser(user: User): Result<User>
+
+    suspend fun getUser(user: User): Result<User?>
+
+    suspend fun updateUser(user: User)
 }
 
 class FirebaseDbDataSource(
@@ -109,5 +118,29 @@ class FirebaseDbDataSource(
                     }
                 }
         }
+    }
+
+    override suspend fun getUser(user: User) = withContext(dispatcher) {
+        return@withContext suspendCoroutine<Result<User?>> { continuation ->
+            database.child("users")
+                .child(user.convertedEmail())
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                        continuation.resume(Result.error(error.toException()))
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val firebaseUser = snapshot.getValue(User::class.java)
+                        continuation.resume(Result.success(firebaseUser))
+                    }
+                })
+        }
+    }
+
+    override suspend fun updateUser(user: User) {
+        val userValues = user.toMap()
+        val childUpdates = HashMap<String, Any>()
+        childUpdates["users/${user.convertedEmail()}"] = userValues
+        database.updateChildren(childUpdates)
     }
 }
