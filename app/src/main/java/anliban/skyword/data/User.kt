968930ba5dee4e30911918ae2f24e28151d8d6a2
BaeJang <qowod77@naver.com>
2019-12-27 02:57:42 +0900

    data 정리

--- app/src/main/java/anliban/skyword/data/User.kt
+++ app/src/main/java/anliban/skyword/data/User.kt
@@ -7,13 +7,19 @@ import com.google.firebase.database.IgnoreExtraProperties
 data class User(
     val email: String = INVALID_EMAIL,
     @Exclude val password: String = INVALID_PASSWORD,
+    val token: String = "",
     val reward: Int = 0
 ) {
     @Exclude
     fun isLogin() =
         email != INVALID_EMAIL && password != INVALID_PASSWORD
 
+    @Exclude
     fun convertedEmail() = email.replace(".", "*")
+
+    @Exclude
+    fun toMap() =
+        mapOf("email" to email, "token" to token, "reward" to reward)
 
     companion object {
         const val INVALID_EMAIL = "email null!!!!!!!!!!"
--- app/src/main/java/anliban/skyword/data/source/local/LocalDataSource.kt
+++ app/src/main/java/anliban/skyword/data/source/local/LocalDataSource.kt
@@ -5,38 +5,38 @@ import anliban.skyword.data.User
 
 interface ILocalDataSource {
 
-    var fcmToken: String?
-    var email: String?
-    var password: String?
+    var fcmToken: String
+    var email: String
+    var password: String
     val user: User
 }
 
 class LocalDataSource(private val pref: PrefModel) : ILocalDataSource {
 
-    override var fcmToken: String?
-        get() = pref.fcmToken
+    override var fcmToken: String
+        get() = pref.fcmToken!!
         set(value) {
             pref.fcmToken = value
         }
 
-    override var email: String?
-        get() = pref.email
+    override var email: String
+        get() = pref.email!!
         set(value) {
             pref.email = value
         }
 
-    override var password: String?
-        get() = pref.password
+    override var password: String
+        get() = pref.password!!
         set(value) {
             pref.password = value
         }
 
     override val user: User
-        get() = User(pref.email!!, pref.password!!)
+        get() = User(email, password, fcmToken)
 }
 
 class PrefModel(context: Context) : PreferenceModel(context) {
-    var fcmToken by stringPreference("fcm_token")
+    var fcmToken by stringPreference("fcm_token", "")
     var email by stringPreference("email", User.INVALID_EMAIL)
     var password by stringPreference("password", User.INVALID_PASSWORD)
 }
